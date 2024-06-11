f8c930968ef7c30ef03302b4fbad9c2a0a10e64c
KanekiEzz <ilyassezzam1337@gmail.com>
2024-06-11 16:25:52 +0100

    add file

--- add_prime_sum/add_prime_sum.c
+++ add_prime_sum/add_prime_sum.c
@@ -1,9 +1,34 @@
 #include <unistd.h>
 
-int ft_is_prime(int  num)
+void ft_putchar(char c)
 {
+    write(1, &c, 1);
+}
+
+void mini_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		write(1, "0", 1);
+        return ;
+	}
+	if (nb >= 10)
+	{
+		mini_putnbr(nb / 10);
+		mini_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+
+int ft_is_prime(int num)
+{
+    if (num <= 1)
+        return 0;
     int j = 2;
-    while(j < num)
+    while (j < num)
     {
         if (num % j == 0)
         {
@@ -14,26 +39,36 @@ int ft_is_prime(int  num)
     return 1;
 }
 
+int mini_atoi(char *str)
+{
+    int num = 0;
+    while (*str >= '0' && *str <= '9')
+    {
+        num = (num * 10) + (*str - '0');
+        str++;
+    }
+    return (num);
+}
+
 int main(int ac, char **av)
 {
     int c = 0;
     int i = 2;
     if (ac == 2)
     {
-        if(av[1][0] >= '0' && av[1][0] <= '9')
+        if (av[1][0] >= '0' && av[1][0] <= '9')
         {
-            int num = av[1][0] - '0';
-
-            while (num <= i)
+            int num = mini_atoi(av[1]);
+            while (num >= i)
             {
-                if(ft_is_prime(num) == 1)
+                if (ft_is_prime(num) == 1)
                 {
-                   c += num;
+                    c += num;
                 }
-                num--;           
+                num--;
             }
         }
-        write(1, &c, 1);
+        mini_putnbr(c);
     }
     else
     {
---
+++ ft_list_size/ft_list_size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct s_list
+{
+  struct s_list *next;
+  void *data;
+} t_list;
+
+int ft_list_size(t_list *begin_list)
+{
+  
+}
+
+int main()
+{
+  t_list list1;
+  t_list list2;
+  t_list list3;
+
+  
+}
