f45c0bce805ac674c6d02bfd824f197cf96a4b27
KanekiEzz <ilyassezzam1337@gmail.com>
2024-06-14 10:12:14 +0100

    14/06/2024

---
+++ repeat_alpha/repeat_alpha.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+	int i = 0;
+	int rep;
+	char c;
+	if (ac == 2)
+	{
+		while (av[1][i] != '\0')
+		{
+			if ((av[1][i] >= 'a' && av[1][i]<= 'z') || (av[1][i] >= 'A' && av[1][i] <= 'Z'))
+			{
+				if (av[1][i] >= 'a' && av[1][i] <= 'z')
+					rep = av[1][i] - 'a';
+				if (c >= 'A' && c <= 'Z')
+					rep = av[1][i] - 'A';
+				while (rep > 0)
+				{
+					write (1, &c, 1);
+					rep--;
+				}
+			}
+			write (1, &c, 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+	return (0);
+}
---
+++ repeat_alpha/repeat_alpha2.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+  int i = 0;
+  int rep;
+  if (ac == 2)
+  {
+    while (av[1][i] != '\0')
+    {
+      if (av[1][i] >= 'a' && av[1][i] <= 'z')
+        rep = av[1][i] - 'a';
+      else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+        rep = av[1][i] - 'A';
+      while (rep > 0)
+      {
+        write(1, &av[1][i], 1);
+        rep--;
+      }
+      write(1, &av[1][i], 1);
+      i++;
+    }
+  }
+  write(1, "\n", 1);
+  return (0);
+}
